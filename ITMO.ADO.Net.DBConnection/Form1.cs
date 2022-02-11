1ce52cd63ad50da390cef6381c673787655f36b4
AydarSE <Galiev_aidar@mail.ru>
2022-02-11 23:37:01 +0300

    Add project files.

---
+++ ITMO.ADO.Net.DBConnection/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ITMO.ADO.Net.DBConnection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection connection = new SqlConnection();
+        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True";
+        private void подключитьсяКБДToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open) 
+                { 
+                    connection.ConnectionString = connectionString;
+                    connection.Open();
+                    MessageBox.Show("Соединение с базой данных " +
+                        connection.Database + "выполнено успешно" + "\nСервер:" +
+                        connection.DataSource); 
+                }
+                else
+                    MessageBox.Show("Соединение с базой данных уже установлено");
+            }
+            catch
+            { 
+                MessageBox.Show("Ошибка соединения с базой данных");
+            }
+        }
+
+        private void отключитьсяОтБДToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close(); MessageBox.Show("Соединение с базой данных закрыто");
+            }
+            else MessageBox.Show("Соединение с базой данных уже закрыто");
+        }
+    }
+}
