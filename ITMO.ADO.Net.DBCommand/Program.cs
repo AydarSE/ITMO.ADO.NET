2a5af17694caed5735cb014ac8d03a485c0e85a5
AydarSE <Galiev_aidar@mail.ru>
2022-02-16 23:36:52 +0300

    Final Update for PW3

--- ITMO.ADO.Net.DBCommand/Program.cs
+++ ITMO.ADO.Net.DBCommand/Program.cs
@@ -16,7 +16,7 @@ namespace ITMO.ADO.Net.DBCommand
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new DBCommand());
         }
     }
 }
