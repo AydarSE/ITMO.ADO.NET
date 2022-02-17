5ea70c47e5e47570f0c8553dc6efaba13bd400c6
AydarSE <Galiev_aidar@mail.ru>
2022-02-17 06:03:30 +0300

    Update

---
+++ ITMO.ADO.NET.DataBindingSimple.P05ex2/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ITMO.ADO.NET.DataBindingSimple.P05ex2
+{
+    partial class DataBinding
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.northwindDataSet = new ITMO.ADO.NET.DataBindingSimple.P05ex2.NorthwindDataSet();
+            this.productsTableAdapter = new ITMO.ADO.NET.DataBindingSimple.P05ex2.NorthwindDataSetTableAdapters.ProductsTableAdapter();
+            this.productsDataGridView = new System.Windows.Forms.DataGridView();
+            this.ProductIDTextBox = new System.Windows.Forms.TextBox();
+            this.ProductNameTextBox = new System.Windows.Forms.TextBox();
+            this.PreviousButto = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // northwindDataSet
+            // 
+            this.northwindDataSet.DataSetName = "NorthwindDataSet";
+            this.northwindDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // productsTableAdapter
+            // 
+            this.productsTableAdapter.ClearBeforeFill = true;
+            // 
+            // productsDataGridView
+            // 
+            this.productsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.productsDataGridView.Location = new System.Drawing.Point(12, 12);
+            this.productsDataGridView.Name = "productsDataGridView";
+            this.productsDataGridView.RowHeadersWidth = 62;
+            this.productsDataGridView.RowTemplate.Height = 28;
+            this.productsDataGridView.Size = new System.Drawing.Size(93, 42);
+            this.productsDataGridView.TabIndex = 1;
+            // 
+            // ProductIDTextBox
+            // 
+            this.ProductIDTextBox.Location = new System.Drawing.Point(27, 293);
+            this.ProductIDTextBox.Name = "ProductIDTextBox";
+            this.ProductIDTextBox.Size = new System.Drawing.Size(428, 26);
+            this.ProductIDTextBox.TabIndex = 2;
+            // 
+            // ProductNameTextBox
+            // 
+            this.ProductNameTextBox.Location = new System.Drawing.Point(27, 359);
+            this.ProductNameTextBox.Name = "ProductNameTextBox";
+            this.ProductNameTextBox.Size = new System.Drawing.Size(428, 26);
+            this.ProductNameTextBox.TabIndex = 3;
+            // 
+            // PreviousButto
+            // 
+            this.PreviousButto.Location = new System.Drawing.Point(27, 408);
+            this.PreviousButto.Name = "PreviousButto";
+            this.PreviousButto.Size = new System.Drawing.Size(134, 53);
+            this.PreviousButto.TabIndex = 4;
+            this.PreviousButto.Text = "Previous";
+            this.PreviousButto.UseVisualStyleBackColor = true;
+            this.PreviousButto.Click += new System.EventHandler(this.PreviousButto_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(27, 485);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 52);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Next";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // DataBinding
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1145, 668);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.PreviousButto);
+            this.Controls.Add(this.ProductNameTextBox);
+            this.Controls.Add(this.ProductIDTextBox);
+            this.Controls.Add(this.productsDataGridView);
+            this.Name = "DataBinding";
+            this.Text = "DataBinding";
+            this.Load += new System.EventHandler(this.DataBinding_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private NorthwindDataSet northwindDataSet;
+        private NorthwindDataSetTableAdapters.ProductsTableAdapter productsTableAdapter;
+        private System.Windows.Forms.DataGridView productsDataGridView;
+        private System.Windows.Forms.TextBox ProductIDTextBox;
+        private System.Windows.Forms.TextBox ProductNameTextBox;
+        private System.Windows.Forms.Button PreviousButto;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ITMO.ADO.NET.DataBindingSimple.P05ex2/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.ADO.NET.DataBindingSimple.P05ex2
+{
+    public partial class DataBinding : Form
+    {
+        public DataBinding()
+        {
+            InitializeComponent();
+        }
+
+        private BindingSource productsBindingSource;
+
+
+        private void DataBinding_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'northwindDataSet.Products' table. You can move, or remove it, as needed.
+            this.productsTableAdapter.Fill(this.northwindDataSet.Products);
+
+            productsTableAdapter.Fill(northwindDataSet.Products);
+
+            productsBindingSource = new BindingSource(northwindDataSet, "Products");
+
+            ProductIDTextBox.DataBindings.Add("Text", productsBindingSource, "ProductID");
+            ProductNameTextBox.DataBindings.Add("Text", productsBindingSource, "ProductName");
+        }
+
+        private void PreviousButto_Click(object sender, EventArgs e)
+        {
+            productsBindingSource.MovePrevious();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            productsBindingSource.MoveNext();
+        }
+    }
+}
