b7965753cf9543ff6be7e3e9237d5bc4768b5d26
AydarSE <Galiev_aidar@mail.ru>
2022-02-17 05:19:04 +0300

    Update P4

---
+++ ITMO.ADO.NET.DatasetDesigner/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ITMO.ADO.NET.DatasetDesigner
+{
+    partial class DataSet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GetCustomersButton = new System.Windows.Forms.Button();
+            this.CustomersListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // GetCustomersButton
+            // 
+            this.GetCustomersButton.Location = new System.Drawing.Point(24, 13);
+            this.GetCustomersButton.Name = "GetCustomersButton";
+            this.GetCustomersButton.Size = new System.Drawing.Size(104, 54);
+            this.GetCustomersButton.TabIndex = 0;
+            this.GetCustomersButton.Text = "Get Customers";
+            this.GetCustomersButton.UseVisualStyleBackColor = true;
+            this.GetCustomersButton.Click += new System.EventHandler(this.GetCustomersButton_Click);
+            // 
+            // CustomersListBox
+            // 
+            this.CustomersListBox.FormattingEnabled = true;
+            this.CustomersListBox.ItemHeight = 20;
+            this.CustomersListBox.Location = new System.Drawing.Point(186, 13);
+            this.CustomersListBox.Name = "CustomersListBox";
+            this.CustomersListBox.Size = new System.Drawing.Size(264, 244);
+            this.CustomersListBox.TabIndex = 1;
+            // 
+            // DataSet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CustomersListBox);
+            this.Controls.Add(this.GetCustomersButton);
+            this.Name = "DataSet";
+            this.Text = "DataSet";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GetCustomersButton;
+        private System.Windows.Forms.ListBox CustomersListBox;
+    }
+}
+
---
+++ ITMO.ADO.NET.WorkingDataTable.P4ex5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.ADO.NET.WorkingDataTable.P4ex5
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new WorkingDT());
+        }
+    }
+}
